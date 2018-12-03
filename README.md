0976c13100b24750c25664831a2e449f36413faa
Gaurav C <cggaurav@gmail.com>
2018-12-03 21:19:05 +0700

    101

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bashme
 Bash 101 + Utilities
+
+# Notes
+
+Name of file denotes what you learn from it
+
+`chmod +x *` | `chmod 755 *`
+
---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+myvariable=Hello
+anothervar=Fred
+this=1
+echo $myvariable
+echo $anothervar 
+echo $this
+
+echo 'this is $this'
+echo "this is $this"
